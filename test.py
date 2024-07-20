6def03a2a405eb6dd954e1b894ea5bcf69e0d462
Symbiosa <103031295+Symbiosa@users.noreply.github.com>
2024-07-20 13:59:27 +0300

    adding tests

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import unittest
 from maze import Maze
+from graphics import Window
 
 class Tests(unittest.TestCase):
     def test_maze_create_cells(self):
+        win = Window(800,600)
         num_cols = 12
         num_rows = 10
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
